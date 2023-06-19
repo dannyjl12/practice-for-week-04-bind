644cfd8757fe54d9a5e6b5cbf5d0b96199d1d77c
danny-lowton <dannylowton@gmail.com>
2023-06-19 18:27:47 -0400

    completed

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-// Your code here
+const Employee = require("./employee");
+
+let johnWick = new Employee("John Wick", "Dog Lover");
+
+const boundEmployee = johnWick.sayName.bind(johnWick);
+global.setTimeout(boundEmployee, 2000);
+
+const boundOccupation = johnWick.sayOccupation.bind(johnWick)
+global.setTimeout(boundOccupation, 3000);
